0f173e12faabc9fcd1c3bf7cfbfab7ba1a41c007
Shijin-Varghese <108483342+Shijin-Varghese@users.noreply.github.com>
2023-02-24 18:41:55 +0530

    Update Order.java

--- Order.java
+++ Order.java
@@ -12,6 +12,18 @@ public class Order {
         }
         return false;
     }
+    public static boolean removeorder(Customer customer, Product product) {
+        try {
+//            String MyOrder="INSERT INTO yourorders(pid, name, price) VALUES(" + product.getId() + "," + product.getName() + ","+product.getPrice()+")";
+            String MyOrder="Delete from yourorders where pid=" + product.getId() + " and cid=" +customer.getId();
+            System.out.println(product.getId()+" "+ customer.getId());
+            DataBaseConnection dbConn = new DataBaseConnection();
+            return dbConn.insertUpdate(MyOrder);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
     public static boolean placeOrder(Customer customer, Product product) {
         try {
             String placeOrder = "INSERT INTO orderss(customer_id, product_id, status) VALUES(" + customer.getId() + "," + product.getId() + ", 'Ordered')";
